f7db6bcc907a16c346e829b6394c7cfefb78704c
netlesh <netlesh@mail.ru>
2020-04-22 09:46:34 +0300

    start

---
+++ app/components/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <Page>
+    <ActionBar>
+      <GridLayout width="100%" columns="auto, *">
+        <Label text="MENU" @tap="$refs.drawer.open(left)" col="0" />
+        <Label class="title" text="Welcome to NativeScript-Vue!" col="1" />
+      </GridLayout>
+    </ActionBar>
+
+    <MultiDrawer>
+      <StackLayout slot="left">
+        <Label text="Im in the left drawer" />
+      </StackLayout>
+
+      <Frame />
+      <Label text="default slot" textWrap="true" />
+      
+      <!-- main content goes into the default slot -->
+    </MultiDrawer>
+  </Page>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      msg: "Hello World!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.title {
+  text-align: left;
+  padding-left: 16;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+
+.drawer-header {
+  padding: 50 16 16 16;
+  margin-bottom: 16;
+  background-color: #53ba82;
+  color: #ffffff;
+  font-size: 24;
+}
+
+.drawer-item {
+  padding: 8 16;
+  color: #333333;
+  font-size: 16;
+}
+</style>
